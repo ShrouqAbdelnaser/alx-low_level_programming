505fda6ccab7c9bcd722fa934d5022480749a94a
ShrouqAbdelnaser <shrouq0510@gmail.com>
2023-10-19 19:01:19 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * @n: pointer
  * Return: n
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 int i;
 
@@ -12,7 +12,7 @@ i = 0;
 while (n[i] != '\0')
 {
 if (n[i] >= 'a' && n[i] <= 'z')
-n[i] = n[i] -32;
+n[i] = n[i] - 32;
 i++;
 }
 return (n);

7b3752bbd5e846ae6c583f8b28e1bc71c46d722f
ShrouqAbdelnaser <shrouq0510@gmail.com>
2023-10-31 16:00:24 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,42 +3,85 @@
 #include <stdio.h>
 
 /**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
-
-char **strtow(char *str)
+ * number - function to calculate number of words
+ * @str: string being passed to check for words
+ * Return: number of words
+ */
+int number(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int c = 0;
+	int a, n = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
-		if (str[i] != 32)
-			c++;
-	}
-
-	d = malloc(sizeof(char) * c);
-
-	if (d == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-		{
-			*d[j] = str[i];
-			j++;
-		}
+		if (*str == ' ')
+			str++;
 		else
 		{
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			n++;
 		}
 	}
-	return (d);
+	return (n);
+}
+
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
+char **strtow(char *str)
+{
+	int tw = 0, b = 0, c = 0, length = 0;
+	char **w, *fw;
+
+	if (str == 0 || *str == 0)
+		return (NULL);
+	tw = number(str);
+	if (tw == 0)
+		return (NULL);
+	w = malloc((tw + 1) * sizeof(char *));
+	if (w == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < tw;)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			fw = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+				length++;
+				str++;
+			}
+			w[b] = malloc((length + 1) * sizeof(char));
+			if (w[b] == 0)
+			{
+				free_everything(w, b);
+				return (NULL);
+			}
+			while (*fw != ' ' && *fw != '\0')
+			{
+				w[b][c] = *fw;
+				fw++;
+				c++;
+			}
+			w[b][c] = '\0';
+			b++; c = 0; length = 0; str++;
+		}
+	}
+	return (w);
 }
